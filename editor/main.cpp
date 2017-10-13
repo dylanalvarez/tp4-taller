a73f3a3f8e6ce3742a989d4185c2a4817b4cc684
Dylan Alvarez <helllo5d123@gmail.com>
2017-10-13 19:55:12 -0300

    example use of yaml-cpp example map.yaml base template for editor screen

---
+++ editor/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "yaml-cpp/yaml.h"
+
+int main()
+{
+    YAML::Emitter out;
+    out << "Hello, World!";
+    std::cout << "Here's the output YAML:\n" << out.c_str();
+    return 0;
+}
