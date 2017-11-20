392aefea727d87000d9321b993524e79c5e56f40
FontiLucas <fontilucas@hotmail.com>
2017-11-20 14:21:54 -0300

    error de sprites

--- src/client/yaml-cpp-release-0.5.1/util/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/geco/Escritorio/Taller 2.0/Tp Final/Pruebas/tp4-taller/src/client/yaml-cpp-release-0.5.1/util
-# Build directory: /home/geco/Escritorio/Taller 2.0/Tp Final/Pruebas/tp4-taller/src/client/yaml-cpp-release-0.5.1/util
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
